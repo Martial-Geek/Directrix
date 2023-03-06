7fd240c34fe063ae54600ebf45cf6ccf9e64036a
Uwais Jawed <uwaisjawed8081@gmail.com>
2023-03-06 23:52:26 +0530

    Authorize Google OAuth

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <GoogleOAuthProvider clientId="491218694600-is3dk5t573u0foet9htr06jv321smkbf.apps.googleusercontent.com">
+        <App />
+    </GoogleOAuthProvider>, document.getElementById("root"));
 
 
